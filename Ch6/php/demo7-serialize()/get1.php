efd8ee98a9a0a1bfadbfa5a7efa35a30d362d2b3
zilong.zhou <zzl81cn@163.com>
2016-06-14 15:37:30 +0800

    add commons css.

--- Ch6/php/demo7-serialize()/get1.php
+++ Ch6/php/demo7-serialize()/get1.php
@@ -1,4 +1,18 @@
 <?php 
 	header("Content-Type:text/html; charset=utf-8");
-	echo "<div class='comment'><h6>{$_REQUEST['username']}:</h6><p class='para'>{$_REQUEST['content']}</p></div>";
+// 	$showtime=date("Y-m-d H:i:s");
+	echo "<div class='comment'>
+			<h6>{$_REQUEST['username']}:</h6>
+			<p class='para'>{$_REQUEST['content']}</p>
+			<p class='para'>{$_REQUEST['number']}</p>
+		</div>";
+	
+// 	echo "<ul>
+// 		<li>
+// 			<div class='comment'><h6>{$_REQUEST['username']}:</h6><p class='para'>{$_REQUEST['content']}</p></div>
+// 		</li>
+// 		<li>
+// 			<div class='comment'><h6>{$_REQUEST['username']}:</h6><p class='para'>{$_REQUEST['content']}</p></div>
+// 		</li>
+// 	</ul>";
 ?>
